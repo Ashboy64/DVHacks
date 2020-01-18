f33f90df379e4e55aab8a02c087d4f252f27b670
Ashish Rao <ashish.arartc@gmail.com>
2020-01-18 13:47:48 -0800

    Sentiment test

---
+++ app/src/main/java/com/example/dvhacks/SentimentTest.java
@@ -0,0 +1,14 @@
+package com.example.dvhacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SentimentTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sentiment_test);
+    }
+}
